f895be6b8f3cd821aea57ceaabecb63eee363866
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2016-02-09 16:24:15 +0100

    Implemented a better way to read the function parameters and extract the values of those parameters, which generalizes the way i create the request per method. Also implemented the rest of the actions with bulk support to be able to perform bulk actions and commit them all at once or clear them

--- pocket.py
+++ pocket.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 
 
@@ -8,22 +9,91 @@ class Pocket:
         self._consumer_key = consumer_key
         self._access_token = access_token
 
-    def retrieve(self, state=None, favorite=None, tag=None, content_type=None,
+        self._bulk_actions = []
+
+    def add(self, url, title=None, tags=None, tweet_id=None):
+        return self._make_request('add')
+
+    def retrieve(self, state=None, favorite=None, tag=None, contentType=None,
                  sort=None, detailType=None, search=None, domain=None,
                  since=None, count=None, offset=None):
-        # This is ugly!
-        vars = locals()
-        del vars['self']
-        return self._make_request('get', **vars)
+        return self._make_request('get')
 
-    def _make_request(self, action, **kwargs):
+    def bulk_commit(self):
+        pass
+
+    def bulk_add(self, item_id, ref_id=None, tags=None,
+                 time=None, title=None, url=None):
+        self._add_action('add')
+        return self
+
+    def archive(self, item_id, time=None):
+        self._add_action('archive')
+        return self
+
+    def readd(self, item_id, time=None):
+        self._add_action('readd')
+        return self
+
+    def favorite(self, item_id, time=None):
+        self._add_action('favorite')
+        return self
+
+    def unfavorite(self, item_id, time=None):
+        self._add_action('unfavorite')
+        return self
+
+    def delete(self, item_id, time=None):
+        self._add_action('delete')
+        return self
+
+    def tags_add(self, item_id, tags, time=None):
+        self._add_action('tags_add')
+        return self
+
+    def tags_remove(self, item_id, tags, time=None):
+        self._add_action('tags_remove')
+        return self
+
+    def tags_replace(self, item_id, tags, time=None):
+        self._add_action('tags_replace')
+        return self
+
+    def tags_clear(self, item_id, time=None):
+        self._add_action('tags_clear')
+        return self
+
+    def tag_rename(self, old_tag, new_tag, time=None):
+        self._add_action('tag_rename')
+        return self
+
+    def reset(self):
+        self._bulk_actions = []
+
+    def commit(self):
+        self._make_request(self._bulk_actions)
+
+    def _add_action(self, action):
+        kwargs = self._get_method_params()
+
+        kwargs['action'] = action
+
+        self._bulk_actions.append(kwargs)
+
+    def _make_request(self, action):
+        if isinstance(action, list):
+            kwargs = {'actions': action}
+            action = 'send'
+        else:
+            kwargs = self._get_method_params()
+
         kwargs.update({
             'consumer_key': self._consumer_key,
             'access_token': self._access_token
         })
 
         response = requests.post(
-            self._get_url('get'),
+            self._get_url(action),
             json=kwargs,
             headers=self._get_headers()
         )
@@ -32,6 +102,16 @@ class Pocket:
             raise self._make_exception(response)
 
         return response.json()
+
+    def _get_method_params(self):
+        caller = sys._getframe(2)
+        var_names = list(caller.f_code.co_varnames)
+        caller_locals = caller.f_locals
+
+        var_names.remove('self')
+        kwargs = {key: value for key, value in caller_locals.items()
+                  if key in var_names and value is not None}
+        return kwargs
 
     def _get_url(self, uri):
         return '{}/{}'.format(Pocket.api_url, uri)
@@ -46,6 +126,7 @@ class Pocket:
         headers = response.headers
 
         return PocketException(
+            response.status_code,
             headers['X-Error-Code'],
             headers['X-Error'],
             headers['X-Limit-User-Limit'],
@@ -58,7 +139,7 @@ class Pocket:
 
 
 class PocketException(Exception):
-    def __init__(self, code, message, user_limit=None,
+    def __init__(self, http_code, error_code, message, user_limit=None,
                  user_remaining=None, user_reset=None,
                  key_limit=None, key_remaining=None, key_reset=None):
         for var_name, value in locals().items():
