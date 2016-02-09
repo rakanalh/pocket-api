2da0d16325f77ceb237ce3a4f7a7d38f684afbf9
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2016-02-09 20:25:01 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='pocket-api',
+    version='0.1.0',
+    author='Rakan Alhneiti',
+    author_email='rakan.alhneiti@gmail.com',
+
+    py_modules=['pocket'],
+
+    # Details
+    url='https://github.com/rakanalh/pocket-api',
+
+    license='LICENSE',
+    description='A python wrapper around GetPocket API V3. ',
+    long_description=open('README.md').read(),
+
+    install_requires=[
+        'requests',
+    ],
+    test_requires=[
+        'pytest'
+        'flake8==2.4.1'
+        'responses==0.5.0'
+    ]
+)
